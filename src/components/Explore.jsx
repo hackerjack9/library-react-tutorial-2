4b142c86ae533e31838dcae505985cd2afa9f9c9
hackerjack9 <lucekl03@frontier.com>
2025-07-18 22:25:34 -0400

    Adding Routes

---
+++ src/components/Explore.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const Explore = () => {
+    return (
+        <section id="explore">
+            <div className="row row__column">
+                <h2>Explore more <span className="purple">Books</span>
+                </h2>
+                <a href="/books">
+                <button className="btn">Explore books</button>
+                </a>
+            </div>
+        </section>
+    )
+}
+
+
+
+
+export default Explore;
